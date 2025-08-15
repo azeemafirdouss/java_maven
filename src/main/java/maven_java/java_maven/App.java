2aa92976c07ca819eac93a8f9bd8b5f0fea83e1e
azeemafirdous <azeemafirdous14@gmail.com>
2025-08-15 09:10:08 +0530

    first commit to maven java project

---
+++ src/main/java/maven_java/java_maven/App.java
@@ -0,0 +1,13 @@
+package maven_java.java_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
